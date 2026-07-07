10f05a7b9d1c184a0de46961ff1119f17e6ed69b
flavioJunior999 <flavio.junior.santos@escola.pr.gov.br>
2026-07-07 07:57:08 -0300

    7.07

---
+++ main.js
@@ -0,0 +1,11 @@
+const botoes = document.querySelectorAll (".botao")
+
+for clet i =0; i ,botoes.length; i++) {
+botoes {1}. onclick = fuction () {
+    for clet j= 0; j < botoes.length; j++){
+        botoes[j].classList.remove("ativo");
+    }
+
+    botoes{j}.classlist.add("ativo");
+};
+}
